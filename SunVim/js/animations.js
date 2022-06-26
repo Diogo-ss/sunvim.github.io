2536e5c7d700d20d8df75c2d177a0fc5433adc4a
Wallace Henrique <98850074+wAAAASH@users.noreply.github.com>
2022-06-26 01:22:04 -0300

    Add files via upload

---
+++ SunVim/js/animations.js
@@ -0,0 +1,9 @@
+// const btnHamburguer = $('#hamburguer_menu');
+// const conteudo = $('#content');
+
+// btnHamburguer.click(() => {
+//     conteudo.animate({
+//         marginLeft: "0%"
+//     },1000);
+
+// })
