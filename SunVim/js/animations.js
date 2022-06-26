d90ca2f04af7c4d12aa2a2bb9f1741f6df32a768
Diogo-ss <diogosantosdasilva6@gmail.com>
2022-06-26 01:25:41 -0300

    final

--- SunVim/js/animations.js
+++
@@ -1,9 +0,0 @@
-// const btnHamburguer = $('#hamburguer_menu');
-// const conteudo = $('#content');
-
-// btnHamburguer.click(() => {
-//     conteudo.animate({
-//         marginLeft: "0%"
-//     },1000);
-
-// })
